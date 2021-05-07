9f54975a03f6ccd457bdc0d567ce738a49823482
daniel.mirandacu@sqasa.co <daniel.mirandacu@sqasa.co>
2021-05-07 09:17:21 -0500

    demo

---
+++ src/test/java/runners/BuscarTemaLibreriaNacionalRunner.java
@@ -0,0 +1,16 @@
+package runners;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/resources/features/BuscarTemaLibreriaNacional.feature",
+glue = "stepsDefinitions",
+snippets = SnippetType.CAMELCASE,
+tags = "@smokeTest")
+public class BuscarTemaLibreriaNacionalRunner {
+
+}
